0a0d7f1e155e8589dccda97ec398406567c87fe7
30604R <minjukim10502@gmail.com>
2025-01-21 18:19:16 +0900

    ViewBinding

---
+++ app/src/main/java/com/example/guru24/activity_password.kt
@@ -0,0 +1,14 @@
+package com.example.guru24
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_password : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password)
+    }
+}
