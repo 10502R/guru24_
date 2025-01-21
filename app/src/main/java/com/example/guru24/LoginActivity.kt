a7c3af9033898f0a579f5119b999875391310024
30604R <minjukim10502@gmail.com>
2025-01-21 19:26:22 +0900

    activity_login

---
+++ app/src/main/java/com/example/guru24/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.guru24
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.guru24.databinding.ActivityLoginBinding
+import com.example.guru24.databinding.ActivitySignupBinding
+
+class LoginActivity : AppCompatActivity() {
+    private var mBinding: ActivityLoginBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
