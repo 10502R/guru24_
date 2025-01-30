ceaeacfee1cc112aea84bf00a2eeda0f2c5792c8
30604R <minjukim30604@gmail.com>
2025-01-30 11:30:46 +0900

    map : 검색 창 기능 구현

--- app/src/main/java/com/example/guru24/SearchActivity.kt
+++ app/src/main/java/com/example/guru24/SearchActivity.kt
@@ -29,7 +29,7 @@ class SearchActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val searchEditText = binding.searchView.findViewById<android.widget.EditText>(
-            androidx.appcompat.R.id.search_src_text
+            R.id.search_text
         )
         searchEditText.setTextColor(resources.getColor(android.R.color.black))
         searchEditText.setHintTextColor(resources.getColor(android.R.color.darker_gray))
