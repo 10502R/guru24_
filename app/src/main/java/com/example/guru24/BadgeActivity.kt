ff08d55195805dc28a15754e028bd5ff4c7988cd
30604R <minjukim30604@gmail.com>
2025-02-01 20:06:31 +0900

    stamp : 도장 깨기 디자인 작업 (1) commit : 뱃지 / 달성시 출력물 디자인 작업 완료

--- app/src/main/java/com/example/guru24/BadgeActivity.kt
+++ app/src/main/java/com/example/guru24/BadgeActivity.kt
@@ -1,23 +1,25 @@
 package com.example.guru24
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guru24.databinding.ActivityBadgeBinding
 
 class BadgeActivity : AppCompatActivity() {
-
-    private var mBinding: ActivityBadgeBinding? = null
-    private val binding get() = mBinding!!
+    private lateinit var binding: ActivityBadgeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivityBadgeBinding.inflate(layoutInflater)
+        binding = ActivityBadgeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mBinding = null // 메모리 누수 방지
+        // "달성 뱃지 확인하기" 버튼 클릭 시 MainActivity로 이동하면서 "뱃지 프래그먼트 + 탭 정보 전달"
+        binding.checkBadgeButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("NAV_INDEX", TrophyFragment) // 네비게이션에서 뱃지 탭으로 이동
+            intent.putExtra("TAB_INDEX", 2) // 뱃지 프래그먼트의 ViewPager2에서 2번 탭 선택
+            startActivity(intent)
+            finish() // 현재 액티비티 종료
+        }
     }
 }
